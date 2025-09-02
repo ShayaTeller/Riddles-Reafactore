662dc4cebe3c746a535b5be5a75be59503ead194
david teller <ydt009@gmail.com>
2025-09-02 12:29:21 +0300

    first init basic files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HomePage from './pages/HomePage'
+
+function App() {
+
+  return (
+    <>
+    <h1>first commit</h1>
+    <HomePage/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <>
+    <h1>welcome to the riddle game HomePage</h1>
+    <button>login</button>
+    <button>singUp</button>
+    <button>Guost</button>
+      
+    </>
+  );
+}
