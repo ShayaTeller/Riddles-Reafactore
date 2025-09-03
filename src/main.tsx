fbe41058e2bbd7ee151ba1b2f248de356c16025f
david teller <ydt009@gmail.com>
2025-09-03 17:23:36 +0300

    build fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import App from "./App.tsx";
 import Login from "./pages/Login.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import Guest from "./pages/Guest.tsx";
-import GameLoop from "./pages/GameLoop.tsx";
 import Menu from "./pages/Menu.tsx";
 import ReadAllRiddles from "./components/ReadAllRiddles.tsx";
 import AddNewRiddle from "./components/NewRiddle.tsx";
@@ -19,7 +18,6 @@ createRoot(document.getElementById("root")!).render(
         <Route path="signin" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="guest" element={<Guest />} />
-        <Route path="startgame" element={<GameLoop />} />
         <Route path="loadboard" element={<h1>not found</h1>} />
         <Route path="menu" element={<Menu/>}/>
         <Route path="readriddles" element={<ReadAllRiddles/>}/>
